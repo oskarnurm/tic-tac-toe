334e29d6bdb2ad2de1d068d79a72adbe81755c8c
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-08-06 23:28:30 +0300

    feat: add almost full working game in console

---
+++ script.js
@@ -0,0 +1,140 @@
+function Gameboard() {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  const getBoard = () => board;
+  const setToken = (row, column, player) => {
+    // board[row][column].getValue()
+    //   ? console.log("Already drawn")
+    //   : board[row][column].addToken(player);
+    if (board[row][column].getValue()) {
+      return false;
+    } else {
+      board[row][column].addToken(player);
+      return true;
+    }
+  };
+
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) =>
+      row.map((cell) => cell.getValue()),
+    );
+    console.log(boardWithCellValues);
+  };
+
+  return { getBoard, setToken, printBoard };
+}
+
+/*
+ ** A Cell represents one "square" on the board and can have one of
+ ** 0: no token is in the square,
+ ** 1: Player One's token,
+ ** 2: Player 2's token
+ */
+
+function Cell() {
+  let value = 0;
+
+  // Accept a player's token to change the value of the cell
+  const addToken = (player) => {
+    value = player;
+  };
+
+  // How we will retrieve the current value of this cell through closure
+  const getValue = () => value;
+
+  return {
+    addToken,
+    getValue,
+  };
+}
+
+function checkWinner(board) {
+  const flatBoard = board.flat().map((cell) => cell.getValue());
+  const winningCombos = [
+    [0, 1, 2], // top row
+    [3, 4, 5], // middle row
+    [6, 7, 8], // bottom row
+    [0, 3, 6], // left column
+    [1, 4, 7], // middle column
+    [2, 5, 8], // right column
+    [0, 4, 8], // diagonal top-left to bottom-right
+    [2, 4, 6], // diagonal top-right to bottom-left
+  ];
+
+  return winningCombos.some((combo) => {
+    return (
+      flatBoard[combo[0]] &&
+      flatBoard[combo[0]] === flatBoard[combo[1]] &&
+      flatBoard[combo[0]] === flatBoard[combo[2]]
+    );
+  });
+}
+
+function GameController(
+  playerOneName = "Player One",
+  playerTwoName = "Player Two",
+) {
+  const board = Gameboard();
+
+  const players = [
+    {
+      name: playerOneName,
+      token: "O",
+    },
+    {
+      name: playerTwoName,
+      token: "X",
+    },
+  ];
+
+  let activePlayer = players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().token}'s turn.`);
+  };
+
+  const playRound = (column, row) => {
+    if (board.setToken(row, column, getActivePlayer().token)) {
+      console.log(
+        `Drawing ${getActivePlayer().name}'s token into ${(column, row)}...`,
+      );
+      // Switch player turn
+      switchPlayerTurn();
+    } else {
+      console.log(`Not allowed to draw over, choose a different cell`);
+    }
+
+    const winner = checkWinner(board.getBoard());
+    if (winner) {
+      console.log(`${getActivePlayer().name} wins!`);
+    } else {
+      printNewRound();
+    }
+  };
+
+  printNewRound();
+
+  // For the console version, we will only use playRound, but we will need
+  // getActivePlayer for the UI version, so I'm revealing it now
+  return {
+    playRound,
+    getActivePlayer,
+  };
+}
+
+const game = GameController();
